e72e5c0b73f88904951f7fba56214e9a4ceb4910
Vorapahct <iordtchap@gmail.com>
2018-04-09 16:41:24 +0200

    Fixed algorithms to work with missing matrix entries

--- algorithm1.py
+++ algorithm1.py
@@ -2,56 +2,42 @@ from mrjob.job import MRJob
 import re
 import os
 import sys
-import time
+import time					
 
 
-def convert_sparse(file, file_name):
-
-	temp_store = open('output.txt','wb')
+def convert_vector(file, file_name):
 	dimensions = file.readline().split()
 	rows = int(dimensions[0])
 	cols = int(dimensions[1])
+	if rows != 1 and cols != 1:
+		return
+	
+	temp_store = open('output_vector.txt','wb')
 	temp_store.write(str(rows) + ' ' + str(cols) + '\r\n')
-	row_number = 0
-	col_number = 0
 
 	with file:
 		for line in file:
 			value = line.split()
 
-			x = int(value[0])
-			y = int(value[1])
-
-			if col_number < cols and x > row_number:
-
-				while col_number < cols :
-					temp_store.write(str(row_number) + ' ' + str(col_number) + ' 0\r\n')
-					col_number += 1
-
-			if int(value[1]) == col_number:
-				temp_store.write(value[0] + ' ' + value[1] + ' ' + value[2] + '\r\n')
-				col_number += 1
-				
-			elif int(value[1]) > col_number:
-				while col_number < y :
-					temp_store.write(str(x) + ' ' + str(col_number) +  ' 0\r\n')
-					col_number += 1
-
-				temp_store.write(value[0] + ' ' + value[1] + ' ' + value[2] + '\r\n')
-				col_number += 1
-
-			if col_number == cols:
-				col_number = 0
-				row_number += 1
+			x = value[0]
+			magnitude = value[1]
+			
+			if cols == 1:
+				temp_store.write(x + ' 0 ' + magnitude + '\r\n')
+			else:
+				temp_store.write('0 ' + x + magnitude + '\r\n')
+	
 	
 	file.close()
 	temp_store.close()
 	
-	with open("output.txt") as file_from:
+	with open("output_vector.txt") as file_from:
 		with open(file_name, "w") as file_to:
 				for line in file_from:
 					file_to.write(line)
-
+	
+	file.close()
+	temp_store.close()
 
 class MRMatrixReduceOneStep(MRJob):
 	
@@ -71,33 +57,35 @@ class MRMatrixReduceOneStep(MRJob):
 				yield (row, int(y)), ('Matrix-identifier-2', int(x), int(value))
 
     def reducer(self, key, val):
-
-		matrix_1_values  = []
-		matrix_2_values  = []
+    
 		answer_matrix    = []
+
+		matrix_1_values = [0] * int(j)
+		matrix_2_values = [0] * int(j)
 
 		for values in val:
 			if values[0] == 'Matrix-identifier-1':
-				matrix_1_values.append(values[2])
+				position = values[1]
+				matrix_1_values[position] = values[2]
 			else:
-				matrix_2_values.append(values[2])
-
+				position = values[1]
+				matrix_2_values[position] = values[2]
+		
 		for iterator in range(0, int(j)):
 			answer_matrix.append(matrix_1_values[iterator] * matrix_2_values[iterator])
 		yield key, sum(answer_matrix)
-
-
-time_start = time.time()
-
-WORD_RE = re.compile(r"[\w']+")
 
 command_line = sys.argv
 
 matrix_1 = open(command_line[-2], 'r')
 matrix_2 = open(command_line[-1], 'r')
 
-convert_sparse(matrix_1,command_line[-2])
-convert_sparse(matrix_2,command_line[-1])
+convert_vector(matrix_1, command_line[-2])
+convert_vector(matrix_2, command_line[-1])
+
+time_start = time.time()
+
+WORD_RE = re.compile(r"[\w']+")
 
 matrix_1 = open(command_line[-2], 'r')
 matrix_2 = open(command_line[-1], 'r')
--- algorithm2.py
+++ algorithm2.py
@@ -6,7 +6,41 @@ import sys
 import time
 
 
-class MRMatrixReduceOneStep(MRJob):
+def convert_vector(file, file_name):
+	dimensions = file.readline().split()
+	rows = int(dimensions[0])
+	cols = int(dimensions[1])
+	if rows != 1 and cols != 1:
+		return
+	
+	temp_store = open('output_vector.txt','wb')
+	temp_store.write(str(rows) + ' ' + str(cols) + '\r\n')
+
+	with file:
+		for line in file:
+			value = line.split()
+
+			x = value[0]
+			magnitude = value[1]
+			
+			if cols == 1:
+				temp_store.write(x + ' 0 ' + magnitude + '\r\n')
+			else:
+				temp_store.write('0 ' + x + magnitude + '\r\n')
+	
+	
+	file.close()
+	temp_store.close()
+	
+	with open("output_vector.txt") as file_from:
+		with open(file_name, "w") as file_to:
+				for line in file_from:
+					file_to.write(line)
+	
+	file.close()
+	temp_store.close()
+
+class MR_matrix_multiplication_TwoStep(MRJob):
 
 	def steps(self):
 		return [MRStep(mapper = self.mapper_first_iteration, reducer = self.reducer_first_iteration),
@@ -46,11 +80,19 @@ class MRMatrixReduceOneStep(MRJob):
 	def reducer_second_iteration(self, key, val):
 		yield key, sum(val)
 
+command_line = sys.argv
+
+matrix_1 = open(command_line[-2], 'r')
+matrix_2 = open(command_line[-1], 'r')
+
+convert_vector(matrix_1, command_line[-2])
+convert_vector(matrix_2, command_line[-1])
+
 timer_start = time.time()
 
 WORD_RE = re.compile(r"[c\w']+")
 
-MRMatrixReduceOneStep.run()
+MR_matrix_multiplication_TwoStep.run()
 
 timer_end = time.time()
 total_time = timer_end - timer_start
